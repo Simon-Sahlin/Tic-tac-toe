ea18153b34fedd7d4dbc137a2b61ca9db4cc00f9
Simon-Sahlin <simonsahlin@somin.se>
2024-03-08 23:31:31 +0100

    Add Basic Game Logic

--- script.js
+++ script.js
@@ -1,1 +1,89 @@
-console.log("Hello World")
+let gameManager = (function(){
+    let turn = 0;
+
+    function addTurn(){
+        turn++;
+        turn = turn % 2;
+    }
+
+    function sign(){
+        return turn == 0 ? "X" : "O"
+    }
+
+    function hasWon(){
+        let combs = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,1,2],
+            [2,4,6],
+        ];
+        for (let comb of combs) {
+            if (
+                gameBoard.getSign(comb[0]) === gameBoard.getSign(comb[1]) &&
+                gameBoard.getSign(comb[1]) === gameBoard.getSign(comb[2]) &&
+                gameBoard.getSign(comb[0]) !== ""
+            )
+                return true;
+        };
+        return false;
+    }
+
+    let makeMove = (id)=>{
+        if (gameBoard.isChecked(id))
+            return;
+        gameBoard.place(id, sign());
+        screenController.updateCell(id);
+        addTurn();
+        if (hasWon())
+            console.log("WIN");
+    }
+
+    return({makeMove})
+})();
+
+let gameBoard = (function(){
+    let cells = [];
+    for (let i = 0; i < 9; i++) {
+        cells.push(new gameCell(i));
+    }
+
+    let isChecked = (id) =>{
+        return cells[id].checked;
+    }
+
+    let getSign = (id) => {
+        return cells[id].sign;
+    }
+
+    let place = (id, sign) =>{
+        cells[id].checked = true;
+        cells[id].sign = sign;
+    }
+    return({cells, isChecked, place, getSign})
+})();
+
+let screenController = (function(){
+
+    gameBoard.cells.forEach(cell => {
+        cell.element.addEventListener("click", ()=>{
+            gameManager.makeMove(cell.id);
+        })
+    });
+
+    let updateCell = (id) =>{
+        gameBoard.cells[id].element.innerHTML = gameBoard.cells[id].sign;
+    }
+
+    return({updateCell})
+})();
+
+function gameCell(id){
+    this.id = id;
+    this.checked = false;
+    this.sign = "";
+    this.element = document.querySelector(`#gameWrapper>div:nth-child(${id+1})`);
+}
